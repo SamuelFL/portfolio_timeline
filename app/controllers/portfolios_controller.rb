3f2bda035324ed97d18b1a62388b1d6b8f7fbe74
SamuelFL <samuel.falcon103@gmail.com>
2018-10-03 09:50:47 +0100

    Custom fields used as ending and due date source.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,5 +3,7 @@ class PortfoliosController < ApplicationController
     @identifier = params[:id]
 	@projectId = Project.select('id').where('identifier = ?', @identifier)
 	@issues = Issue.where(project_id: @projectId)
+	@plannedStartDateID = CustomField.select('id').find_by name: 'Fecha inicio planificada' 
+	@plannedDueDateID = CustomField.select('id').find_by name: 'Fecha fin planificada'
   end
 end
