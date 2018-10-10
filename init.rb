74bc1f540ecb8422e192c7895e3f4805eaec6668
SamuelFL <samuel.falcon103@gmail.com>
2018-10-10 11:21:43 +0100

    Style ammends

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :portfolio_timeline do
   author 'SamuelFL'
   description 'Redmine plugin for portfolio management'
   version '0.1.4'
-  project_module :portfolios do
+  project_module :portfolio do
     permission :view_portfolios, :portfolios => :index
   end
   menu :project_menu, :portfolios, { :controller => 'portfolios', :action => 'index' }, :caption => 'Portfolio', :after => :overview
