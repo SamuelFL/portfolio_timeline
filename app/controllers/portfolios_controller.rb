5a227579e27063b9c40d5c511b21cf02cf787a9e
SamuelFL <samuel.falcon103@gmail.com>
2018-10-18 11:44:44 +0100

    added moment library to change ocale to spanish + some minor refactor

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,8 +4,8 @@ class PortfoliosController < ApplicationController
 	@projectId = Project.select('id').where('identifier = ?', @identifier)
 	@projectName = Project.select('name').find_by id: @projectId
 	@issues = Issue.where(project_id: @projectId)
-	@plannedStartDateID = CustomField.select('id').find_by name: 'Fecha inicio planificada' 
-	@plannedDueDateID = CustomField.select('id').find_by name: 'Fecha fin planificada'
+	@startDateID = CustomField.select('id').find_by name: 'Fecha inicio planificada' 
+	@dueDateID = CustomField.select('id').find_by name: 'Fecha fin planificada'
 	@hitosFieldID = CustomField.select('id').find_by name: 'Hitos'
   end
 end
