3dd6d8fd92da41b02c2e6893a7d6e2f61d58a96d
SamuelFL <samuel.falcon103@gmail.com>
2018-10-31 13:35:46 +0000

    Update visibility for elements function generalized

--- assets/javascripts/plugin.js
+++ assets/javascripts/plugin.js
@@ -1,43 +1,30 @@
-function updateHitos(){
-  var hitosArray= document.getElementsByClassName('vis-point');
-  var checkBox = document.getElementById('hitosCheckbox');
-  if(checkBox.checked){
-    showHitos(hitosArray);
-  }else{
-	hideHitos(hitosArray);
-  }
-  timeline.redraw();
+function updateElement(element){
+	switch(element){
+		case "progressBar":
+			var elementArray= document.getElementsByClassName('progress-wrapper');
+			var checkBox = document.getElementById('progressBarCheckbox');
+			break;
+		case "hitos":
+			var elementArray= document.getElementsByClassName('vis-point');
+			var checkBox = document.getElementById('hitosCheckbox');
+			break;
+	}
+	if(checkBox.checked){
+		showElement(elementArray);
+	}else{
+		hideElement(elementArray);
+	}
+	timeline.redraw();
 }	
-function hideHitos(hitosArray){
-  for (var i=0; i<hitosArray.length;i++){
-    hitosArray[i].style.display='none';
-  }
+function hideElement(elementArray){
+	for (var i=0; i<elementArray.length;i++){
+		elementArray[i].style.display='none';
+	}
 }
-function showHitos(hitosArray){
-  for (var i=0; i<hitosArray.length;i++){
-    hitosArray[i].style.display='inline-block';
-  }
-}
-
-function updateProgressBar(){
-  var progressBarArray= document.getElementsByClassName('progress-wrapper');
-  var checkBox = document.getElementById('progressBarCheckbox');
-  if(checkBox.checked){
-    showProgressBar(progressBarArray);
-  }else{
-	hideProgressBar(progressBarArray);
-  }
-  timeline.redraw();
-}	
-function hideProgressBar(progressBarArray){
-  for (var i=0; i<progressBarArray.length;i++){
-    progressBarArray[i].style.display='none';
-  }
-}
-function showProgressBar(progressBarArray){
-  for (var i=0; i<progressBarArray.length;i++){
-    progressBarArray[i].style.display='inline-block';
-  }
+function showElement(elementArray){
+	for (var i=0; i<elementArray.length;i++){
+		elementArray[i].style.display='inline-block';
+	}
 }
 
 function moveWindow(startArray){
