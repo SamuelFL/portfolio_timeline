a7393878e212f32a21303131e2dbb3f73a5acbf7
SamuelFL <samuel.falcon103@gmail.com>
2018-10-31 13:16:12 +0000

    Minor fix in applyFilter() function

--- assets/javascripts/plugin.js
+++ assets/javascripts/plugin.js
@@ -8,18 +8,16 @@ function updateHitos(){
   }
   timeline.redraw();
 }	
-
 function hideHitos(hitosArray){
   for (var i=0; i<hitosArray.length;i++){
     hitosArray[i].style.display='none';
   }
-};
-
+}
 function showHitos(hitosArray){
   for (var i=0; i<hitosArray.length;i++){
     hitosArray[i].style.display='inline-block';
   }
-};
+}
 
 function updateProgressBar(){
   var progressBarArray= document.getElementsByClassName('progress-wrapper');
@@ -31,18 +29,16 @@ function updateProgressBar(){
   }
   timeline.redraw();
 }	
-
 function hideProgressBar(progressBarArray){
   for (var i=0; i<progressBarArray.length;i++){
     progressBarArray[i].style.display='none';
   }
-};
-
+}
 function showProgressBar(progressBarArray){
   for (var i=0; i<progressBarArray.length;i++){
     progressBarArray[i].style.display='inline-block';
   }
-};
+}
 
 function moveWindow(startArray){
 	var range = timeline.getWindow();
@@ -59,21 +55,16 @@ function applyFilter(){
 	var selector = document.getElementById('statusSelector');
 	var status = selector.options[selector.selectedIndex].text;
 	var items = groups.get();
-	if(status == "-- Seleccione estado --"){
-		for(var i=0;i<items.length;i++){
-			items[i].visible= true;
-		}
-	}else{
+	if(status != "-- Seleccione estado --"){
 		for(var i=0;i<items.length;i++){
 			if(items[i].status != status){
 				items[i].visible= false;
 			}
 		}
-	}
-	var shownGroups = new vis.DataSet();
-	shownGroups.add(items);
-	timeline.setGroups(shownGroups);
-}
-function clearFilter(){
-	window.location.reload();
+		var shownGroups = new vis.DataSet();
+		shownGroups.add(items);
+		timeline.setGroups(shownGroups);
+	}else{
+		timeline.setGroups(groups);
+	}	
 }
