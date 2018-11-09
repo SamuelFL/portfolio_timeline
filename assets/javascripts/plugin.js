c3f8beeee30e0fa433723ac931c8d2f35f38b0b8
SamuelFL <samuel.falcon103@gmail.com>
2018-11-09 12:02:32 +0000

    Status filter now allows multiple selection(ammend)

--- assets/javascripts/plugin.js
+++ assets/javascripts/plugin.js
@@ -59,7 +59,6 @@ function applyTrackerFilter(){
 	updateElement("progressBar");
 	updateElement("hitos");
 }
-
 function setStatusSelectorOptions(trackerId){
 	var currentTrackerArray = statusPerTrackerArray[trackerId];
 	var optionsAsString = "";
@@ -71,35 +70,6 @@ function setStatusSelectorOptions(trackerId){
 	}
 	$("select[id='statusSelector']").find('option').remove().end().append($(optionsAsString));
 }
-
-/*function applyStatusFilter(){
-	var selector = document.getElementById('statusSelector');
-	var status = selector.options[selector.selectedIndex].text;
-	if(trackerFilteredGroups.length > 0){
-		var items = trackerFilteredGroups.get();
-	}else{
-		var items = groups.get();
-	}
-	if(status != "-- Seleccione estado --"){
-		for(var i=0;i<items.length;i++){
-			if(items[i].status != status){
-				items[i].visible= false;
-			}
-		}
-		var statusFilteredGroups = new vis.DataSet();
-		statusFilteredGroups.add(items);
-		timeline.setGroups(statusFilteredGroups);
-	}else{
-		if(trackerFilteredGroups.length > 0){
-			timeline.setGroups(trackerFilteredGroups);
-		}else{
-			timeline.setGroups(groups);
-		}
-	}	
-	updateElement("progressBar");
-	updateElement("hitos");
-}*/
-
 function applyStatusFilter(){
 	var statuses = $('#statusSelector').val();
 	
@@ -130,7 +100,6 @@ function applyStatusFilter(){
 	updateElement("progressBar");
 	updateElement("hitos");
 }
-
 
 function zoomIn(){
 	timeline.zoomIn(0.85);
